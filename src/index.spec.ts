a7b2e35b6c53e559f4b99a53697db95d6e539870
kurone-kito <krone@kit.black>
2019-10-02 22:05:01 +0900

    Added the unit tests for root methods

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,24 @@
 import { render } from '.';
+import { EnhancedResume } from '~/entities/enhanced';
 
-jest.mock('./container', () => ({ get: () => false }));
+const mockHandle = jest.fn<Promise<Buffer>, EnhancedResume[]>(async input =>
+  Buffer.from(JSON.stringify(input))
+);
+jest.mock('./container', () => ({ get: () => ({ handle: mockHandle }) }));
 
-describe('dummy', () => {
-  it('test', () => expect(render({})).toBeTruthy());
+describe('render() method: mock container.get() => { handle: async input => toBuffer(input) }', () => {
+  describe.each<EnhancedResume>([
+    { basics: { name: 'foo' } },
+    { basics: { name: 'bar' } }
+  ])('input: %o', input => {
+    beforeEach(() => mockHandle.mockClear());
+    it('The method bypasses an argument to the handle inner method.', () => {
+      render(input);
+      expect(mockHandle).toBeCalledWith(input);
+    });
+    it('The handle inner method gets buffered object.', async () => {
+      const expected = Buffer.from(JSON.stringify(input));
+      expect(expected.equals(await render(input)));
+    });
+  });
 });
