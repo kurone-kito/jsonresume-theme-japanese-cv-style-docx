301591347a0ffe96de49f3b22890b5ed690d25e2
kurone-kito <krone@kit.black>
2019-10-14 01:55:38 +0900

    Added a usecase

---
+++ src/usecases/TransformResume.ts
@@ -0,0 +1,33 @@
+import { inject, injectable } from 'inversify';
+import 'reflect-metadata';
+import { EnhancedResume, OutputResume } from '../entities/enhanced';
+import createActivity from '../entities/createActivity';
+import groupBySkills from '../entities/groupBySkills';
+
+export const TransformResumeUseCaseType = Symbol.for('TransformResumeUseCase');
+
+export interface TransformResumeUseCase {
+  handle: (input: EnhancedResume) => Promise<Buffer>;
+}
+
+export const TransformPresenterType = Symbol.for('TransformPresenter');
+
+export interface TransformPresenter {
+  transform: (output: OutputResume) => Promise<Buffer>;
+}
+
+@injectable()
+export class TransformResumeInteractor implements TransformResumeUseCase {
+  private readonly presenter: TransformPresenter;
+
+  constructor(@inject(TransformPresenterType) presenter: TransformPresenter) {
+    this.presenter = presenter;
+  }
+
+  public readonly handle = ({ skills, ...input }: EnhancedResume) =>
+    this.presenter.transform({
+      ...input,
+      activities: createActivity(input),
+      skills: groupBySkills(skills)
+    });
+}
