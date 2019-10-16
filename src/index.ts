73943723cb01f98305d82fca936149e23c894bed
kurone-kito <krone@kit.black>
2019-10-16 10:11:12 +0900

    Improved the entry point

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-export const render = (resume: object) => JSON.stringify(resume);
+import { EnhancedResume } from './entities/enhanced';
+import {
+  TransformResumeUseCase,
+  TransformResumeUseCaseType
+} from './usecases/TransformResume';
+import Container from './container';
+
+export const render = (input: EnhancedResume) =>
+  Container.get<TransformResumeUseCase>(TransformResumeUseCaseType).handle(
+    input
+  );
 export default render;
