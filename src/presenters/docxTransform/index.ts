d80b5c5a57074a58480a779fa481a39a7fb44742
kurone-kito <krone@kit.black>
2019-10-18 17:16:25 +0900

    Added a heading

--- src/presenters/docxTransform/index.ts
+++ src/presenters/docxTransform/index.ts
@@ -4,12 +4,13 @@ import 'reflect-metadata';
 import { OutputResume } from '../../entities/enhanced';
 import { TransformPresenter } from '../../usecases/TransformResume';
 import createDocument from './createDocument';
+import renderHeading from './renderHeading';
 
 @injectable()
 export default class DOCXPresenter implements TransformPresenter {
   transform = ({ basics, meta }: OutputResume) => {
     const doc = createDocument(basics, meta);
-    doc.addSection({ children: [] });
+    doc.addSection({ children: [...renderHeading({ basics, meta })] });
 
     return Packer.toBuffer(doc);
   };
