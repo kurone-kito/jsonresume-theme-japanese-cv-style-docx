f46bb995af647c4d75cd9711dafcf7cc11090734
kurone-kito <krone@kit.black>
2019-10-20 00:32:01 +0900

    Added a suffix

--- src/presenters/docxTransform/index.ts
+++ src/presenters/docxTransform/index.ts
@@ -5,6 +5,7 @@ import { OutputResume } from '../../entities/enhanced';
 import { TransformPresenter } from '../../usecases/TransformResume';
 import createDocument from './createDocument';
 import renderHeading from './renderHeading';
+import renderSuffix from './renderSuffix';
 import renderSummary from './renderSummary';
 
 @injectable()
@@ -12,7 +13,11 @@ export default class DOCXPresenter implements TransformPresenter {
   transform = ({ basics, meta }: OutputResume) => {
     const doc = createDocument(basics, meta);
     doc.addSection({
-      children: [...renderHeading({ basics, meta }), ...renderSummary(basics)]
+      children: [
+        ...renderHeading({ basics, meta }),
+        ...renderSummary(basics),
+        ...renderSuffix()
+      ]
     });
 
     return Packer.toBuffer(doc);
