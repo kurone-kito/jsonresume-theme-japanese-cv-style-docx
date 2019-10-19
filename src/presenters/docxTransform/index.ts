cd4a52fe9b2eaf5f7dbbb4265f40400d94562d16
kurone-kito <krone@kit.black>
2019-10-19 14:51:04 +0900

    Added a summary

--- src/presenters/docxTransform/index.ts
+++ src/presenters/docxTransform/index.ts
@@ -5,12 +5,15 @@ import { OutputResume } from '../../entities/enhanced';
 import { TransformPresenter } from '../../usecases/TransformResume';
 import createDocument from './createDocument';
 import renderHeading from './renderHeading';
+import renderSummary from './renderSummary';
 
 @injectable()
 export default class DOCXPresenter implements TransformPresenter {
   transform = ({ basics, meta }: OutputResume) => {
     const doc = createDocument(basics, meta);
-    doc.addSection({ children: [...renderHeading({ basics, meta })] });
+    doc.addSection({
+      children: [...renderHeading({ basics, meta }), ...renderSummary(basics)]
+    });
 
     return Packer.toBuffer(doc);
   };
