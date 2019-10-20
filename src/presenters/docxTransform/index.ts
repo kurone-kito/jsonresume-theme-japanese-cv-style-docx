7ee1208bce69b08eadb1ec0be803d4d5dbc01d28
kurone-kito <krone@kit.black>
2019-10-21 00:22:22 +0900

    Added the projects

--- src/presenters/docxTransform/index.ts
+++ src/presenters/docxTransform/index.ts
@@ -5,17 +5,19 @@ import { OutputResume } from '../../entities/enhanced';
 import { TransformPresenter } from '../../usecases/TransformResume';
 import createDocument from './createDocument';
 import renderHeading from './renderHeading';
+import renderProjects from './renderProjects';
 import renderSuffix from './renderSuffix';
 import renderSummary from './renderSummary';
 
 @injectable()
 export default class DOCXPresenter implements TransformPresenter {
-  transform = ({ basics, meta }: OutputResume) => {
+  transform = ({ basics, meta, projects }: OutputResume) => {
     const doc = createDocument(basics, meta);
     doc.addSection({
       children: [
         ...renderHeading({ basics, meta }),
         ...renderSummary(basics),
+        ...renderProjects(projects),
         ...renderSuffix()
       ]
     });
