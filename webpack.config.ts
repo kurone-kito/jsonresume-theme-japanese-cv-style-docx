f79a66cc407b3fc678f7b006ca46ded155ee442a
kurone-kito <krone@kit.black>
2019-10-30 14:01:01 +0900

    Implemented the path alias

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,45 +1,55 @@
+import path from 'path';
+import 'ts-polyfill/lib/es2019-object';
 import webpack from 'webpack';
-import packageJson from './package.json';
+import { dependencies, name } from './package.json';
+import { compilerOptions } from './tsconfig.json';
 
 const DtsBundleWebpack = require('dts-bundle-webpack');
+
+const createAliases = () => {
+  const baseUrl = path.resolve(__dirname, compilerOptions.baseUrl || '.');
+
+  return Object.fromEntries(
+    Object.entries(compilerOptions.paths || {}).map(([key, [value]]) => [
+      key.replace('*', ''),
+      path.resolve(baseUrl, value.replace('*', ''))
+    ])
+  );
+};
 
 export default (source: webpack.Configuration): webpack.Configuration => ({
   ...source,
   cache: true,
   devtool: false,
-  externals: Object.keys(packageJson.dependencies || {}),
+  externals: Object.keys(dependencies || {}),
   mode: 'production',
   module: {
     rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader'
-      },
+      { test: /\.tsx?$/, use: 'ts-loader' },
       {
         test: /\.tsx?$/,
         enforce: 'pre',
         loader: 'eslint-loader',
-        options: {
-          configFile: '.eslintrc.yml'
-        }
+        options: { configFile: '.eslintrc.yml' }
       }
     ]
   },
   output: {
     filename: 'index.js',
     path: __dirname,
-    library: packageJson.name,
+    library: name,
     libraryTarget: 'umd'
   },
   plugins: [
     new DtsBundleWebpack({
       indent: '  ',
       main: 'src/index.d.ts',
-      name: packageJson.name,
+      name,
       out: '../index.d.ts'
     })
   ],
   resolve: {
+    alias: createAliases(),
     extensions: ['.js', '.json', '.ts', '.tsx']
   },
   target: 'node'
